aba8ef57431f206d7088ddc595055c025d0220ca
Emtiaz khan <emtiaz287@gmail.com>
2023-03-30 02:51:17 +0600

    Everything is ready

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import HOOKS_USESTATE1 from './HOOKS_USESTATE/index1'
+import HOOKS_USESTATE2 from './HOOKS_USESTATE/index2'
+
+export default function App() {
+  return (
+    <div>
+        <HOOKS_USESTATE2 />
+    </div>
+  )
+}
